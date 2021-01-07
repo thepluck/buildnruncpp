2202c47509794ab7a1eca4bb8ecacc23c92a9b34
Nguyễn Hoàng Vũ <54402720+thepluck@users.noreply.github.com>
2021-01-07 16:19:02 +0700

    Update build.cpp

--- build.cpp
+++ build.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-/// build taskName
+/// Ubuntu: build taskName
+/// Windows: ./build.exe taskName
 int main(int argc, char *argv[]) {
     string cmd = "g++ -std=c++14 -Wall -Wextra -Wfloat-equal -Wshadow "
     + string(argv[1]) + ".cpp -o " + string(argv[1]);
