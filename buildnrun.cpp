f1cf88330d53dc27e9ee99eeb08c0ac74f211b59
Nguyễn Hoàng Vũ <54402720+thepluck@users.noreply.github.com>
2021-01-10 15:29:11 +0700

    Update buildnrun.cpp

--- buildnrun.cpp
+++ buildnrun.cpp
@@ -7,6 +7,14 @@ int main(int argc, char *argv[]) {
         puts("buildnrun [taskName]\nOptions:\n    -i [inputFile]\t\tread input from [inputFile]\n    -o [outputFile]\t\twrite output to [outputFile]\n"); return 0;
     }
     char cmd[1000] = "build "; strcat(cmd, argv[1]);
-    strcat(cmd, " run "); strcat(cmd, argv[1]);
+    strcat(cmd, "; run "); strcat(cmd, argv[1]);
+    if (argc > 3) {
+        strcat(cmd, " "); strcat(cmd, argv[2]);
+        strcat(cmd, " "); strcat(cmd, argv[3]);
+    }
+    if (argc > 5) {
+        strcat(cmd, " "); strcat(cmd, argv[4]);
+        strcat(cmd, " "); strcat(cmd, argv[5]);
+    }
     system(cmd); return 0;
 }
