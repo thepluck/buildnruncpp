3af8027fa193d54f146f2b1c3df0aee1caa87035
Nguyễn Hoàng Vũ <54402720+thepluck@users.noreply.github.com>
2021-01-10 13:21:22 +0700

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -1,15 +1,23 @@
-/// Usage: taskName (inputFile) (outputFile)
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     char cmd[1000] = "./"; strcat(cmd, argv[1]);
-    if (argc > 2) {
-        strcat(cmd, " < "); strcat(cmd, argv[2]);
-    }
     if (argc > 3) {
-        strcat(cmd, " > "); strcat(cmd, argv[3]);
+        if (string(argv[2]) == "-i")
+            strcat(cmd, " < ");
+        else if (string(argv[2]) == "-o")
+            strcat(cmd, " > ");
+        strcat(cmd, argv[3]);
+    }
+    if (argc > 5) {
+        if (string(argv[4]) == "-i")
+            strcat(cmd, " < ");
+        else if (string(argv[4]) == "-o")
+            strcat(cmd, " > ");
+        strcat(cmd, argv[5]);
+
     }
     system(cmd); system("echo; echo");
 }
