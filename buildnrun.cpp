0f34a44612c0ba6a561e0d8bebbd5fc9a7861772
Nguyễn Hoàng Vũ <54402720+thepluck@users.noreply.github.com>
2021-01-10 13:21:01 +0700

    Update buildnrun.cpp

--- buildnrun.cpp
+++ buildnrun.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-/// buildnrun taskName (inputFile) (outputFile)
 int main(int argc, char *argv[]) {
     if (string(argv[1]) == "--help") {
         puts("buildnrun [taskName]\nOptions:\n    -i [inputFile]\t\tread input from [inputFile]\n    -o [outputFile]\t\twrite output to [outputFile]\n"); return 0;
